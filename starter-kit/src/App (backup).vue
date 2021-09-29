5b11ed9c59e77583c08d56524ba6f1f670968c73
Eric Oliveira <70595549+eoliveira-sesc@users.noreply.github.com>
2021-09-29 18:05:41 -0300

    Reorganização de repositórios
    Integração do Starter Kit ao repositório Design System, com criação de versionamentos

---
+++ starter-kit/src/App (backup).vue
@@ -0,0 +1,35 @@
+<template>
+	<v-app>
+		<botao>Texto</botao>
+		<botao class="alerta">Texto</botao>
+		<botao class="cancelar" ref="botao">Texto</botao>
+		<botao class="confirmar" @click="funcao" :loadingX="false">Texto</botao>
+	</v-app>
+</template>
+
+<style lang="scss">
+	#app {
+		background-color: #111A20 !important;
+	}
+</style>
+
+<script>
+	import botao from "./design-system/components/botao/botao.vue";
+
+	export default {
+		name: "App",
+
+		components: {
+			botao
+		},
+
+		methods: {
+			funcao: function(event) {
+				alert(event.target.getAttribute('loadingX'));
+				alert(this.loadingX);
+
+				// this.$refs.botao.loading = false;
+			}
+		}
+	};
+</script>
---
+++ starter-kit/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+	<v-app>
+		<botao>Texto</botao>
+		<botao class="alerta">Texto</botao>
+		<botao class="cancelar" ref="botao">Texto</botao>
+		<botao class="confirmar" :loading="loading" @click="remove">Texto</botao>
+
+		<v-btn
+			:loading="loading"
+			class="ma-1"
+			color="error"
+			plain
+			@click="remove"
+		>
+		Delete
+		</v-btn>
+	</v-app>
+</template>
+
+<style lang="scss">
+	#app {
+		background-color: #111A20 !important;
+	}
+</style>
+
+<script>
+	import botao from "./design-system/components/botao/botao.vue";
+		
+	export default {
+		name: "App",
+
+		components: {
+			botao
+		},
+
+		methods: {
+			funcao() {
+			},
+
+			async remove () {
+				this.loading = true;
+
+				await new Promise(resolve => setTimeout(resolve, 3000));
+
+				this.loading = false;
+			},
+		},
+  }
+</script>
